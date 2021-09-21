7978a2c427b64207df2ef3df040252eb0b0dfb25
Michal Maciag <maciag@smartpatient.eu>
2021-09-21 17:35:58 +0200

    Change default sorcery source to modified repo

--- Sources/SwiftyMockyCLICore/Utils/Global.swift
+++ Sources/SwiftyMockyCLICore/Utils/Global.swift
@@ -2,8 +2,8 @@ import Foundation
 import PathKit
 
 public var kSwiftyMockyCommand = Path("mocky")
-public var kSourceryVersion = "1.0.2"
-public var kDefaultSourceryCommand = Path("mint run krzysztofzablocki/Sourcery@\(kSourceryVersion) sourcery")
+public var kSourceryVersion = "1.5.3"
+public var kDefaultSourceryCommand = Path("mint run michal-maciag/Sourcery@\(kSourceryVersion) sourcery")
 
 public protocol AutoMockable {}
 
